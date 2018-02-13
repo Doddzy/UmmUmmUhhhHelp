75483c93379b5140192a209d6e0f6248f2740081
Effy Elden <git@effy.is>
2018-02-13 17:04:44 +1100

    Test that get_cat_fact returns a fact about a cat

--- spec/lib/umm_umm_uhhh_help_spec.rb
+++ spec/lib/umm_umm_uhhh_help_spec.rb
@@ -7,4 +7,10 @@ describe UmmUmmUhhhHelp do
       expect(subject).to be_a(String)
     end
   end
+  describe '.get_cat_fact' do
+    subject { described_class.get_cat_fact }
+    it "should return a fact containing the word 'cat'" do
+      expect(subject).to include('cat')
+    end
+  end
 end
