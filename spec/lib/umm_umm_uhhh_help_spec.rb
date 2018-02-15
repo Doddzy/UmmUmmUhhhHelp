d4ef3509ce3bb45ccd5568e3107cf24634c9e82d
Claire Dodd <doddzy1991@gmail.com>
2018-02-15 12:14:29 +1100

    Fix another flaky test that was calling API, mock out request to be consistent

--- spec/lib/umm_umm_uhhh_help_spec.rb
+++ spec/lib/umm_umm_uhhh_help_spec.rb
@@ -1,8 +1,16 @@
 require 'umm_umm_uhhh_help'
 
 describe UmmUmmUhhhHelp do
+  stubbed_external_fact = 'Cats see six times better in the dark and at night than humans.'
+
+  before do
+    json_fact = { fact: stubbed_external_fact, length: 63 }.to_json
+
+    stub_request(:get, 'https://catfact.ninja/fact').to_return(body: json_fact)
+  end
+
   describe '.get_suggestion' do
-    subject {described_class.get_suggestion}
+    subject { described_class.get_suggestion }
 
     it 'should return a suggestion' do
       expect(subject).to be_a(String)
@@ -12,13 +20,8 @@ describe UmmUmmUhhhHelp do
   describe '.get_cat_fact' do
     subject {described_class.get_cat_fact}
 
-    it "should return a fact containing the word 'cat'" do
-      fact = "Cats see six times better in the dark and at night than humans."
-      json_fact = { fact: fact, length: 63 }.to_json
-
-      stub_request(:get, "https://catfact.ninja/fact").to_return(body: json_fact)
-
-      expect(subject).to eql(fact)
+    it 'should return stubbed cat fact' do
+      expect(subject).to eql(stubbed_external_fact)
     end
   end
 end
